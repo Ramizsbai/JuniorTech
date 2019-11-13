0c03e978ab0a13dab7a7ed00dd3d6a1ca792efbd
Ahmad <hamadagh777@gmail.com>
2019-11-13 11:13:20 +0100

    adding header component

---
+++ src/headerComponent/header.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import "./header.css"
+
+class header extends Component {
+    render() {
+        return (
+            <div className="bg-image">
+                
+            </div>
+        )
+    }
+}
+
+export default header
