0d677659914deaa835dbdb4dc0ac92ed80afb327
temo <tamimabotafish@gmail.com>
2019-11-21 15:54:30 +0100

    new add

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import Header from './home/header/headerComponents/Header';
 import './app.css';
+import Footer from './home/footer/Footer'
 
 function App() {
   return (
     <div className="App">
-    
-    <Header />
+
+      <Header />
+
+      <Footer />
+
     </div>
   );
 }
