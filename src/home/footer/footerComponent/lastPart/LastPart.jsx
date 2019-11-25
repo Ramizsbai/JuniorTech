1e2ff225bb9c3b558257e8aed029cfb5d1ab158c
temo <tamimabotafish@gmail.com>
2019-11-25 16:24:43 +0100

    add last part in the footer

---
+++ src/home/footer/footerComponent/lastPart/LastPart.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import './lastPart.css';
+const Example = (props) => {
+    return (
+        <div className="LastPart">
+            <Container>
+                <Row>
+                        
+                </Row>
+            </Container >
+
+
+
+
+        </div>
+    );
+}
+export default Example;
