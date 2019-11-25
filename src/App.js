987296d0401d94f41782926282c274a098c3584e
temo <tamimabotafish@gmail.com>
2019-11-25 14:35:42 +0100

    new commit

--- src/App.js
+++ src/App.js
@@ -1,43 +1,31 @@
 import React from 'react';
 import './app.css';
 
-<<<<<<< HEAD
-=======
+
 import Home from './home/Home';
 
 import Navbar from './navbar/Navbar';
-import {BrowserRouter, Route, Switch} from 'react-router-dom';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Jobs from './Jobs/Jobs';
 import About from './about/About';
 
->>>>>>> 88c36e00d5b251a759463acfe52f84736c0c84ca
+
 
 function App() {
   return (
-        <BrowserRouter>
+    <BrowserRouter>
 
-    <div className="App">
+      <div className="App">
+        <Navbar />
+        <Switch>
+          <Route path='/' component={Home} exact />
+          <Route path='/jobs' component={Jobs} />
+          <Route path='/about' component={About} />
 
-<<<<<<< HEAD
-      <Header />
+        </Switch>
 
-ggit
-====
-    
-    
-    
-
-         <Navbar />
-         <Switch>
-           <Route path='/' component={Home} exact/> 
-           <Route path='/jobs' component={Jobs}/> 
-           <Route path='/about' component={About}/> 
-
-         </Switch>
->>>>>>> 88c36e00d5b251a759463acfe52f84736c0c84ca
-
-    </div>
-    </BrowserRouter>   
+      </div>
+    </BrowserRouter>
 
   );
 }
