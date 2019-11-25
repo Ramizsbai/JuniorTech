a2d6e287eeba466f7a17f9db974d428aff4f985e
temo <tamimabotafish@gmail.com>
2019-11-25 10:54:53 +0100

    new add

--- src/home/footer/Footer.jsx
+++ src/home/footer/Footer.jsx
@@ -42,17 +42,6 @@ function Footer() {
                     </ul>
                 </div>
                 <div className="col">
-                    <h1>Resources</h1>
-                    <ul>
-                        <li>Webmail</li>
-                        <li>Redeem code</li>
-                        <li>WHOIS lookup</li>
-                        <li>Site map</li>
-                        <li>Web templates</li>
-                        <li>Email templates</li>
-                    </ul>
-                </div>
-                <div className="col">
                     <h1>Support</h1>
                     <ul>
                         <li>Contact us</li>
