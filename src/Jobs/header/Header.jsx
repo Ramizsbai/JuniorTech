9e2a826d0acfc1b5cf410128d0462d3e614570bb
temo <tamimabotafish@gmail.com>
2019-12-02 12:51:05 +0100

    adding new CSS header

--- src/Jobs/header/Header.jsx
+++ src/Jobs/header/Header.jsx
@@ -1,27 +1,19 @@
 import React, { Component } from 'react';
-import { Button } from 'reactstrap'
-import './header.css'
+import { Button } from 'reactstrap';
+import './header.css';
 
 export class Header extends Component {
     render() {
         return (
             <div className="jobs-header">
-                <div className="input-content">
-                    <form className="awesome-form d-flex">
-                        <div className="header-container">
-                            <div className="input-group">
-                                <input type="text" placeholder="Job title"></input>
-                            </div>
-
-                            <div className="input-group">
-                                <input type="text" placeholder="Locations" ></input>
-                            </div>
-                            <Button className="searchButton" color="primary">Search</Button>
-                        </div>
-                    </form>
+                <div className="job-header">
+                    <input type="text" placeholder="Job Title"></input>
+                    <input type="text" placeholder="Location"></input>
+                    <Button className="d-flex" color="success">Search</Button>
                 </div>
-
             </div>
+
+
         );
     }
 }
