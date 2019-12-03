6d09f387db80557e8a2f8152a64a035bb7f3a661
Ahmad <hamadagh777@gmail.com>
2019-12-03 14:40:22 +0100

    adding fake json data for testing

--- src/Jobs/Jobs.jsx
+++ src/Jobs/Jobs.jsx
@@ -2,9 +2,8 @@ import React, { Component } from 'react';
 import Header from './header/Header';
 import Body from './body/Body'
 
-
-
 export class Jobs extends Component {
+
     render() {
         return (
             <div>
--- src/Jobs/body/bodyComponents/JobsComponent/JobsComponent.jsx
+++ src/Jobs/body/bodyComponents/JobsComponent/JobsComponent.jsx
@@ -3,24 +3,26 @@ import './jobsComponent.css'
 import JobsSubComponent from './jobsSubComponent/JobsSubComponent';
 
 export class Jobs extends Component {
+    
+        state = {
+         users: [],
+        }
+    
+
+    componentDidMount(){
+        fetch('https://jsonplaceholder.typicode.com/users')
+        .then(response => response.json())
+        .then(data => {
+            this.setState({users : data})
+            console.log(this.state)
+        })
+        .catch(console.log)
+    }
     render() {
         return (
             <div className="jobs-container" id="style-1">
-                <JobsSubComponent />
-                <JobsSubComponent />
-                <JobsSubComponent />
-                <JobsSubComponent />
-                <JobsSubComponent />
-                <JobsSubComponent />
-                <JobsSubComponent />
-                <JobsSubComponent />
-                <JobsSubComponent />
-                <JobsSubComponent />
-                <JobsSubComponent />
-                <JobsSubComponent />
-                <JobsSubComponent />
-                <JobsSubComponent />
-                <JobsSubComponent />
+                <JobsSubComponent users={this.state.users} />
+
 
             </div>
         )
