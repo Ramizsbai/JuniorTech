19bfe8c71a0955e66a0a8dc96c1d38c7a5865736
temo <tamimabotafish@gmail.com>
2019-12-03 15:49:55 +0100

    stayling footer

--- src/home/footer/Footer.jsx
+++ src/home/footer/Footer.jsx
@@ -3,16 +3,33 @@ import './footer.css';
 import { Button } from 'reactstrap';
 
 
+
 export class Footer extends Component {
     render() {
         return (
             <div className="home-footer">
-                <h5 id="Subscribe">Subscribe Now !</h5>
-                <div className="home-footer2 mx-auto">
-                    <input className="input-footer my-auto" type="text" placeholder="Email address"></input>
-                    <Button className="d-flex my-auto" color="success">Send</Button>
-                </div>
 
+                <h2 id="Subscribe"> Subscribe Now ! </h2>
+                <div className="home-footer2">
+                    <input className="input-footer" type="text" placeholder="Email address"></input>
+                    <Button className="d-flex ml-3 my-auto" color="success">Send</Button>
+                </div>
+                
+                <div className="contact-footer">
+                    <h4 >Contact Us</h4>
+                    <ul>
+                        <li id="col">Contact Us</li>
+                        <li id="col">Contact Us</li>
+                        <li id="col">Contact Us</li>
+                    </ul>
+
+                    <h4>Contact Us</h4>
+                    <ul>
+                        <li id="col">Contact Us</li>
+                        <li id="col">Contact Us</li>
+                        <li id="col">Contact Us</li>
+                    </ul>
+                </div>
             </div>
         )
     }
