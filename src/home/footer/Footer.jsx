1a137b0efa49f0ff98243cb34bea092483553745
temo <tamimabotafish@gmail.com>
2019-12-04 14:31:14 +0100

    adding amendment to footer

--- src/home/footer/Footer.jsx
+++ src/home/footer/Footer.jsx
@@ -12,10 +12,27 @@ export class Footer extends Component {
                 <h2 id="Subscribe"> Subscribe Now ! </h2>
                 <div className="home-footer2">
                     <input className="input-footer" type="text" placeholder="Email address"></input>
-                    <Button className="d-flex ml-3 my-auto" color="success">Send</Button>
+                    <Button className="button-footer d-flex ml-3 my-auto" color="success">Send</Button>
+                </div>
+                <div className="flex-row">
+                    <ul className="footer-list">
+
+                        <h4 className="footer-list1">Contact Us</h4>
+
+                        <li>contact us</li>
+                        <li>contact us</li>
+                        <li>contact us</li>
+                    </ul>
+                    <ul className="footer-list">
+
+                        <h4 className="footer-list1">Contact Us</h4>
+
+                        <li>contact us</li>
+                        <li>contact us</li>
+                        <li>contact us</li>
+                    </ul>
                 </div>
 
-                
                 <div className="LastPart mx-auto container">
                     <div className="copyright">
                         <p className="my-auto">Cobyright by: Stonres</p>
