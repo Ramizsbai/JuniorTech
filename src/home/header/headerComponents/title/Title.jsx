58a8bf69f0e885cc4f7f0b32052eaca4c7093300
Ahmad <hamadagh777@gmail.com>
2019-12-08 23:13:17 +0100

    adding new font family

--- src/home/header/headerComponents/title/Title.jsx
+++ src/home/header/headerComponents/title/Title.jsx
@@ -6,7 +6,7 @@ export class Title extends Component {
     render() {
         return (
             <div className="headerTitle">
-                <p className="first-header text-center">Your <span className="first-step">first step</span> in tech career</p>
+                <p className="first-header text-center">Your first step in <span className="Tech">Tech</span> career</p>
                 <p className="second-header text-center mt-1">find your first job in tech industry</p>
             </div>
         )
