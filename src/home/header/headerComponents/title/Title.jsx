327cdf7e82dfc010e95ecaba27933b7d49cbbbb1
Ahmad <hamadagh777@gmail.com>
2019-12-08 17:38:30 +0100

    adding media query to title in home page

--- src/home/header/headerComponents/title/Title.jsx
+++ src/home/header/headerComponents/title/Title.jsx
@@ -6,8 +6,8 @@ export class Title extends Component {
     render() {
         return (
             <div className="headerTitle">
-                <p className="h1 text-center">Your <span className="first-step">first step</span> in tech career</p>
-                <p className="h5 text-center mt-2">find your first job in tech industry</p>
+                <p className="first-header text-center">Your <span className="first-step">first step</span> in tech career</p>
+                <p className="second-header text-center mt-1">find your first job in tech industry</p>
             </div>
         )
     }
