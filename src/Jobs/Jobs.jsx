4811221e1316a1acf59171e969aa93f7b7655d0f
delgadosuarez <delgado.suarez@outlook.com>
2019-12-09 14:42:34 +0100

    Creating filter

--- src/Jobs/Jobs.jsx
+++ src/Jobs/Jobs.jsx
@@ -1,11 +1,14 @@
 import React, { Component } from 'react';
 import Header from './header/Header';
 
+import Filter from './body/filterComponent/filter'
+
 export class Jobs extends Component {
     render() {
         return (
             <div>
                 <Header />
+                <Filter />
             </div>
         )
     }
---
+++ src/Jobs/body/Body.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import Filter from './filterComponent/Filter';
+
+
+export class Body extends Component {
+    render() {
+        return (
+            <div>
+                <Filter />
+            </div>
+        )
+    }
+}
+
+export default Body
